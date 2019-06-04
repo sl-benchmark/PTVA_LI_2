93b41978bf7d1a025fc0034e63cc44ddf8aec522
stanislas jouven <stanislasjouven@gmail.com>
2019-06-04 22:27:44 +0200

    Added all the files from PTVA_LI

---
+++ test_source_loc.py
@@ -0,0 +1,32 @@
+import networkx as nx
+import numpy as np
+import sys
+
+import PTVA_LI.source_estimation as se
+import operator
+
+from multiprocessing import Pool
+
+### Compute a batch in parallel
+def ptva_li(graph, obs_time, distribution) :
+
+    mu = distribution.mean()
+    sigma = distribution.std()
+    obs = np.array(list(obs_time.keys()))
+
+    path_lengths = {}
+    paths = {}
+    for o in obs:
+        path_lengths[o], paths[o] = nx.single_source_dijkstra(graph, o)
+
+    ### Run the estimation
+    s_est, likelihoods, d_mu, cov = se.ml_estimate(graph, obs_time, sigma, mu, paths,
+        path_lengths)
+
+    ranked = sorted(likelihoods.items(), key=operator.itemgetter(1), reverse=True)
+
+    return (s_est, ranked)
+
+
+
+# -----------------------------------------------------------------------------
